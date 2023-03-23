9ea6f963ba50acf19177f510d2f58fdd2c9f5f49
Jacques Gariépy <jacques.gariepy@outlook.com>
2023-03-23 16:55:36 -0400

    Create NFTERC721A.sol

---
+++ NFTERC721A.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "./ERC721A.sol";
+
+contract NFTIsERC721A is Ownable, ERC721A {
+    using Strings for uint;
+
+    constructor() ERC721A("Jackboy70", "JCK") {}
+
+    function mint(uint256 quantity) external onlyOwner {
+        _mint(msg.sender, quantity);
+    }
+
+    function tokenURI(uint _tokenId) public view virtual override returns (string memory) {
+        require(_exists(_tokenId), "URI query for nonexistent token");
+
+        return string(abi.encodePacked("", _tokenId.toString(), ".json"));
+    }
+}

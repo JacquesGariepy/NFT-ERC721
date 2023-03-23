addabf5db4e6b6d4336436837afff06f9995bf9f
Jacques Gariépy <jacques.gariepy@outlook.com>
2023-03-23 17:09:02 -0400

    Update NFTERC721A.sol

--- NFTERC721A.sol
+++ NFTERC721A.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.17;
+pragma solidity >=0.8.2 <0.9.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
